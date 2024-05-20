8b1152ae58a170cc307f7fb3c4d59797f27602bc
Max <149390445+maxtpv@users.noreply.github.com>
2024-05-21 01:04:50 +0300

    commit

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'meksbackend.nomoredomainswork.ru';
+export const BASE_URL = 'https://meksbackend.nomoredomainswork.ru';
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
